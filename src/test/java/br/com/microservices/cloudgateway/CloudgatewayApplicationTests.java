33187091e10990716edb61372c82cab7cf6caf83
oRonold <or040918@gmail.com>
2025-10-13 17:09:57 -0300

    setup cloud gateway

---
+++ src/test/java/br/com/microservices/cloudgateway/CloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservices.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
